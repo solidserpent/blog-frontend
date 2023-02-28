17b2952d89661a64ee3cf2ceb8b221a7650b347e
solidserpent <37921848+solidserpent@users.noreply.github.com>
2023-02-28 18:23:48 -0500

    Update Home.js
    Changed header of home page

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
   }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Brian Blog Home</h1>
       <div className="row">
         {postsInState.map((post) => {
           return (
